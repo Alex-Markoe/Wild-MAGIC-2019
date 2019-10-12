503a7c277a51c1d4a64673a1f6e6b45528264b5c
Ike McNutt (RIT Student) <irm8232@ad.rit.edu>
2019-10-12 11:19:30 -0400

    PlayerMovement
    Implement the basic player movement.

---
+++ Wild_MAGIC_2019/Assets/Scripts/PlayerStuff/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("Player Settings")]
+    public float hp = 3f;
+    public float movementSpeed = 3f;
+    public float attackDamage = 1f;
+
+    [Space(5)]
+    [Header("Player's Scripts")]
+    public PlayerMovement pMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pMove.movementSpeed = movementSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    {
+
+    }
+
+    public void TakeDamage(float amt)
+    {
+        hp -= amt;
+    }
+}
---
+++ Wild_MAGIC_2019/Assets/Scripts/PlayerStuff/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 3f;
+
+    private Rigidbody2D rb;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        else
+            direction = Vector3.zero;
+
+        if (direction.magnitude > 1)
+            direction = direction.normalized;
+    }
+
+    public Vector3 GetDirection()
+    {
+        return direction;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + (direction * movementSpeed * Time.deltaTime));
+    }
+}
