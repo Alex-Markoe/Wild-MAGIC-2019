d5619b53a99d5d1b336b715ac0d6051115e12ccb
Sam Deane (RIT Student) <scd9799@ad.rit.edu>
2019-10-12 19:54:56 -0400

    Tarot cards and transitions

---
+++ Wild_MAGIC_2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Player playerPrefab;
+    public Player player;
+    public TarotCardManager cardManager;
+    public EnemyManager enemyManager;
+
+    private int sceneIndex;
+    private static CardType playerChosen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if(sceneIndex == 2)
+        {
+            player = Instantiate<Player>(playerPrefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sceneIndex == 1)
+        {
+            playerChosen = cardManager.CardChosen();
+            if (playerChosen != CardType.None)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Destroy(cardManager.cardList[0].GetComponent<SpriteRenderer>());
+                    Destroy(cardManager.cardList[0]);
+                    cardManager.cardList.RemoveAt(0);
+                }
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        if(sceneIndex == 2)
+        {
+            if (player.hp <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
---
+++ Wild_MAGIC_2019/Assets/Scripts/UIStuff/ButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public int indexOfTransition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        indexOfTransition = SceneManager.GetActiveScene().buildIndex;
+        if(indexOfTransition == 2)
+        {
+            indexOfTransition = 0;
+        }
+        else
+        {
+            indexOfTransition++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(indexOfTransition == -1)
+        {
+            Application.Quit();
+        }
+        SceneManager.LoadScene(indexOfTransition);
+    }
+}
