b510c8332127ecdd42d44455ecd09e71ae978cae
Ike McNutt (RIT Student) <irm8232@ad.rit.edu>
2019-10-13 13:26:30 -0400

    Boss Stuff

---
+++ Wild_MAGIC_2019/Assets/Scripts/BossStuff/BossBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+    public float damage;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Wall")
+        {
+            if(collision.transform.GetComponent<Player>())
+            {
+                collision.transform.GetComponent<Player>().TakeDamage(damage);
+            }
+            Destroy(this.gameObject);
+        }       
+    }
+}
